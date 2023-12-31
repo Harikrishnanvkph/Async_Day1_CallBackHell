43c721728f2f2f21fcb303a63e44d4dbb1ffe800
Harikrishnanvkph <harikrishnanvk0531@gmail.com>
2023-12-31 12:40:17 +0530

    async1

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementsByTagName("h1")[0].innerText = "10"
+    setTimeout(()=>{
+        document.getElementsByTagName("h1")[0].innerText = "9"
+        setTimeout(()=>{
+            document.getElementsByTagName("h1")[0].innerText = "8"
+            setTimeout(()=>{
+                document.getElementsByTagName("h1")[0].innerText = "7"
+                setTimeout(()=>{
+                    document.getElementsByTagName("h1")[0].innerText = "6"
+                    setTimeout(()=>{
+                        document.getElementsByTagName("h1")[0].innerText = "5"
+                        setTimeout(()=>{
+                            document.getElementsByTagName("h1")[0].innerText = "4"
+                            setTimeout(()=>{
+                                document.getElementsByTagName("h1")[0].innerText = "3"
+                                setTimeout(()=>{
+                                    document.getElementsByTagName("h1")[0].innerText = "2"
+                                    setTimeout(()=>{
+                                        document.getElementsByTagName("h1")[0].innerText = "1"
+                                        setTimeout(()=>{
+                                            document.getElementsByTagName("h1")[0].innerText = `Happy New Year \u{1F604}`;
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
